ae5b3ff3b46a0a50b45afebd168d753d564036ff
whyzaa <892903912@qq.com>
2019-04-11 14:17:12 +0800

    配置WeChat 用于获取openId

---
+++ src/main/java/com/simpleaccount/configuration/AuthCode2Session.java
@@ -0,0 +1,26 @@
+package com.simpleaccount.configuration;
+
+/**
+ * @Description: java类作用描述
+ * @Author: Whyza
+ * @CreateDate: 2019/4/11 12:56
+ * @Version: 1.0
+ */
+public class AuthCode2Session {
+    /**
+     * 请求的网址
+     */
+    public static final String WX_LOGIN_URL = "https://api.weixin.qq.com/sns/jscode2session";
+    /**
+     * appid
+     */
+    public static final String WX_LOGIN_APPID = "wxfd2ebef9dd9999af";
+    /**
+     * 密匙
+     */
+    public static final String WX_LOGIN_SECRET = "01a3baf10e773c18951cd73842af747e";
+    /**
+     * 固定参数
+     */
+    public static final String WX_LOGIN_GRANT_TYPE = "authorization_code";
+}
