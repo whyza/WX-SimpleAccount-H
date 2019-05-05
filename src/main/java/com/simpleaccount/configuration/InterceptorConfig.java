18da9ff4ebe71588d88301b3a45b5e393d0f3b17
whyzaa <892903912@qq.com>
2019-05-05 23:42:04 +0800

    update

--- src/main/java/com/simpleaccount/configuration/InterceptorConfig.java
+++ src/main/java/com/simpleaccount/configuration/InterceptorConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import static sun.tools.jconsole.Messages.ALL;
 
 /**
  * @Description: java类作用描述
