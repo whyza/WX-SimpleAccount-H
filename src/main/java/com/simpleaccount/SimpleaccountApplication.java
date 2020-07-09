19b66efa9e0a632cb29e8e63265e17f3d74f4cd0
whyza <892903912@qq.com>
2020-07-09 21:43:29 +0800

    去除无用import

--- src/main/java/com/simpleaccount/SimpleaccountApplication.java
+++ src/main/java/com/simpleaccount/SimpleaccountApplication.java
@@ -1,24 +1,10 @@
 package com.simpleaccount;
 
-import com.simpleaccount.controller.UserController;
-import com.simpleaccount.websocket.LoggerMessage;
+
 import org.mybatis.spring.annotation.MapperScan;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-
-import javax.annotation.PostConstruct;
-import java.util.Properties;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 /**
  *
  * @Description: 启动类
